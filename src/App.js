74ced2f24d4ecae4ab351a6dabe42965bd4754a6
Vlad Berezin <berezinva@outlook.com>
2021-10-06 09:10:27 +0300

    attributes learn

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from "react";
+import Table from "./Table";
 
 class App extends React.Component{
     render(){
+
+        
+
+        const characters = [
+            {
+                name:   'Charlie',
+                job:    'janitor',
+            },
+            {
+                name:   'Mac',
+                job:    'Bouncer',
+            },
+            {
+                name:   'Dee',
+                job:    'Aspiring Actress',
+            },
+            {
+                name:   'Dennis',
+                job:    'Bartender',
+            }
+        ]
+
+        const headerCustom = [
+            
+                'JoooB',
+                'NAme',
+            
+        ]
+
+        const myAttrib = {
+            characters,
+            headerCustom,
+        }
+
         return(
-            <div className = "App">
-                <h1>Hello abr!</h1>
+            <div className = "container">
+                <Table mya = {myAttrib}  />
             </div>
         );
     }
--- src/Table.js
+++ src/Table.js
@@ -1,5 +1,42 @@
 import React, {Component} from "react";
 
 class Table extends Component{
+    render(){
+
+        const {mya} = this.props; //prop definition.
+
+        return(
+            <table>
+                <TableHeader hehe = {mya}/>
+                <TableBody lala = {mya} />
+            </table>
+        );
+    }
+}
+
+const TableHeader = (props) => {
+    return(
+        <thead>
+            <tr>
+                <th>{props.hehe.headerCustom[0]}</th>
+                <th>{props.hehe.headerCustom[1]}</th>
+            </tr>
+        </thead>
+    );
+}
+
+const TableBody = (props) => {
+    const rows = props.lala.characters.map((row, index) => {
+        return (
+            <tr key = {index}>
+                <td>{row.name}</td>
+                <td>{row.job}</td>
+            </tr>
+        )
+    });
+
+    return <tbody>{rows}</tbody>
     
 }
+
+export default Table;
